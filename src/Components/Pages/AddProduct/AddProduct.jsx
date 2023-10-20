3e3835dcf7acf80a8e4a7e28f443bc0565a2f8e3
sabbir1791 <sabbirh8181@gmail.com>
2023-10-20 18:53:46 +0600

    dark mode and card bg update

--- src/Components/Pages/AddProduct/AddProduct.jsx
+++ src/Components/Pages/AddProduct/AddProduct.jsx
@@ -51,7 +51,7 @@ const AddProduct = () => {
         }));
       };
   return (
-    <div className="bg-white dark:bg-gray-900">
+    <div className=" dark:bg-gray-900">
       <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
         <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
           Add a new product
