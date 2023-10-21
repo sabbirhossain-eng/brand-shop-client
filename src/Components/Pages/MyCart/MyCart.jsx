e89f56428f284fa666e6dc65d88e55cc45efbee2
sabbir1791 <sabbirh8181@gmail.com>
2023-10-21 23:33:38 +0600

    updates

--- src/Components/Pages/MyCart/MyCart.jsx
+++ src/Components/Pages/MyCart/MyCart.jsx
@@ -5,21 +5,28 @@ import CartCard from "./CartCard";
 const MyCart = () => {
     const loadedProduct = useLoaderData();
     const [products, setProducts] = useState(loadedProduct);
-    
-
 
     return (
         <div className="grid md:grid-cols-3 gap-4">
         {
-            products?.map(product =><CartCard
-            key={product._id}
-            product={product}
-            products={products}
-            setProducts={setProducts}
-            ></CartCard>)
+            products ? (
+                products.map(product => (
+                    <CartCard
+                        key={product._id}
+                        product={product}
+                        products={products}
+                        setProducts={setProducts}
+                    ></CartCard>
+                ))
+            ) : 
+            (
+                <div>
+                    <h2 className="text-2xl font-bold text-center items-center">Please Add Cart</h2>
+                </div>
+            )
         }
       </div>
     );
-};
+}
 
 export default MyCart;
